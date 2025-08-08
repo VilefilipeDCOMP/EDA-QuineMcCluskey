9b620ee384937d3cffa64decc75d4a410b415720
Filipe Ciríaco <filipe.donascimento@dcomp.ufs.br>
2025-08-08 19:48:13 -0300

    Versão corrigida

--- McCluskey.c
+++ McCluskey.c
@@ -4,16 +4,12 @@
 
 int globalArray[256];
 int globalArrayQtd = 0;
-int entrada;
-int DEBUG_Contador = 0;
 
 typedef struct no {
     char binario[20];
     int numeros1;
     int cobertura[32];
     int coberturaQtd;
-    int foiUtilizado;
-    int foiPrintado;
     struct no *prox;
 } tipoNo;
 
@@ -27,8 +23,6 @@ void inicializa (tipoLista* lista) {
     lista->qtd = 0;
     lista->inicio = NULL;
     lista->fim = NULL;
-    int coberturaQtd = 0;
-    int foiUtilizado = 0;
 }
 
 void adicionarLista (tipoLista* lista, char binarioP[10], int numerosP, int cobertura[32], int coberturaQtd) {
@@ -80,36 +74,12 @@ void ordenarNum1 (tipoLista *lista){
     }while(troca);
 }
 
-int buscaArray(int *soma, int size, int elemento) {
-    for (int i=0; i < size; i++) {
-        if (soma[i] == elemento){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int buscaLista(tipoLista *lista, char binario[20]) {
-    tipoNo* aux = lista->inicio;
-    for (int i=0; i < lista->qtd; i++) {
-        if (strcmp(aux->binario, binario) == 0){ 
-            return 1;
-        }
-        aux = aux->prox;
-    }
-    return 0;
-}
-
 int comparador (char *bit1, char *bit2, char* result) {
     int tamanho = strlen(bit1);
     int aux = 0;
-    int numBarras = 0;
     
     for(int i=0; i < tamanho; i++){
         if (bit1[i] != bit2[i]) {
-            if (bit1[i] == '-') {
-                numBarras++;
-            }
             aux++;
         }
     }
@@ -120,47 +90,35 @@ int comparador (char *bit1, char *bit2, char* result) {
     
     int auxNumeros1 = 0;
     for(int i=0; i < tamanho; i++){
-        if(bit1[i] == bit2[i] && bit1[i] != '-'){
+        if(bit1[i] == bit2[i]){
             result[i] = bit1[i];
             if (result[i] == '1') {
                 auxNumeros1++;
             }
-        } 
-        // else if((bit1[i] == '-' || bit2[i] == '-') || (entrada/2 <= numBarras)){
-        //     return -1;
-        //     // bit1[i] = bit1[i+1];
-        //     // bit2[i] = bit2[i+1];
-        // }
-        else {
+        } else {
             result[i] = '-';
         }
     }  
     return auxNumeros1;
 }
 
-int printar(char *binaro, tipoLista *lista){
+void printar(char *binaro){
     char letra = 'A';
-    if (buscaLista(lista, binaro) == 0) {
-        for(int i =0; i<strlen(binaro); i++){
-            if(binaro[i] == '-') continue;
-    
-            if(binaro[i] == '0'){
-                printf("~");
-            }
-            printf("%c", letra + i);
-            adicionarLista(lista, binaro, -1, NULL, -1);
-            return 1;
+
+    for(int i =0; i<strlen(binaro); i++){
+        if(binaro[i] == '-') continue;
+
+        if(binaro[i] == '0'){
+            printf("~");
         }
+        printf("%c", letra + i);
     }
-    return 0;
 }
 
 void escolhaCobertura (tipoLista *lista) {
     tipoNo *aux = lista->inicio;
     int novoArray[32]; 
-    int print = 0;
-    tipoLista listaJaPrintada;
-    inicializa(&listaJaPrintada);
+    int print =0;
     
     int contadorNOVOARRAY;
     for (int i = 0; i < lista->qtd; i++) {
@@ -179,12 +137,12 @@ void escolhaCobertura (tipoLista *lista) {
                 aux2 = aux2->prox;
             }
             if (achou == 0) {
-                // printf("únicos");
                 if(print) printf(" + ");
-                print = printar(aux->binario, &listaJaPrintada);
-                // print = 1;
+                printar(aux->binario);
+                print = 1;
 
                 for (int i = 0; i < aux->coberturaQtd; i++) {
+                    // printf("%i ", aux->cobertura[i]);
                     for (int j=0; j < globalArrayQtd; j++) {
                         if (aux->cobertura[i] == globalArray[j]) {
                             globalArray[j] = -1;
@@ -206,16 +164,12 @@ void escolhaCobertura (tipoLista *lista) {
         if (globalArray[i] != -1) {
             while (aux != NULL) {
                 for (int j=0; j < aux->coberturaQtd; j++) {
-                    if (globalArray[i] != 0) {
-                        // printf("%i %i\n", aux->cobertura[j], globalArray[i]);
-                        if (aux->cobertura[j] == globalArray[i]) {
-                            if(print) printf(" + ");
-                            print = printar(aux->binario, &listaJaPrintada);
-                            globalArray[i] = 0;
-                            // print = 1;
-                            achou = 1;
-                            break;
-                        }
+                    if (aux->cobertura[j] == globalArray[i]) {
+                        // printf("%s\n", aux->binario);
+                        if(print) printf(" + ");
+                        printar(aux->binario);
+                        print = 1;
+                        achou = 1;
                     }
                 }
                 if (achou == 1) {
@@ -224,27 +178,65 @@ void escolhaCobertura (tipoLista *lista) {
                 aux = aux->prox;
             }
         }
-        // if (achou == 1) {
-        //     break;
+        if (achou == 1) {
+            break;
+        }
+
+        // while (aux != NULL) {
+        //     for (int j=0; j < aux->coberturaQtd; j++) {
+        //         if (globalArray[i] == aux->cobertura[j]) {
+        //             if (globalArray[i + 1] != aux->cobertura[j + 1]) {
+        //                 achou = 1;
+        //             }
+        //         } else {
+        //             valorFalta = aux->cobertura[j];
+        //         }
+        //         printf("%d %d %d\n", globalArray[i], aux->cobertura[j], achou);
+        //     }
+        //     aux = aux->prox;
+        // }
+        // if (achou == 0) {
+        //     printf("Valor: %d\n", valorFalta);
         // }
     }
 }
 
-
+// void escolhaCobertura (tipoLista *lista) {
+//     tipoNo *aux = lista->inicio;
+//     int novoArray[32]; 
+//     int contadorNOVOARRAY;
+//     for (int i = 0; i < lista->qtd; i++) {
+//         tipoNo* aux2 = lista->inicio;
+//         int achou = 0;
+//         for (int m = 0; m < aux->coberturaQtd; m++)
+//             for (int j = 0; j < lista->qtd; j++) {
+//             {
+//                 for (int n = 0; n < aux2->coberturaQtd; n++)
+//                 {   
+//                     if (aux->cobertura != aux2->cobertura) {
+//                         if (aux->cobertura[n] == aux2->cobertura[m]) {
+//                             achou = 1;
+//                         }
+//                         printf("%d %d %d\n", aux->cobertura[m], aux2->cobertura[n], achou);
+//                     }
+//                 }
+//             }
+//             aux2 = aux2->prox;
+//         }
+//         if (achou == 1) {
+//             printf("%s\n", aux->binario);
+//         }
+//         aux = aux->prox;
+//     }
+// }
 
 int mergeArray(int *cobertura1, int *cobertura2, int size1, int size2, int *soma) {
     int k = 0;
     for (int i=0; i < size1; i++) {
-        if (buscaArray(soma, size1, cobertura1[i]) == 0) {
-            soma[k++] = cobertura1[i];
-        }
-        // soma[k++] = cobertura1[i];
+        soma[k++] = cobertura1[i];
     }
     for (int j=0; j < size2; j++) {
-        if (buscaArray(soma, size2, cobertura2[j]) == 0) {
-            soma[k++] = cobertura2[j];
-        }
-        // soma[k++] = cobertura2[j];
+        soma[k++] = cobertura2[j];
     }
     return k;
 }
@@ -264,6 +256,7 @@ int algoritmoComparar (tipoLista *lista, tipoLista *complexodemais) {
     tipoNo* aux = lista->inicio;
     int pfvfunciona = 0;
     if(aux == NULL) return 0;
+
     for (int i = 0; i < lista->qtd; i++) {
         tipoNo* aux2 = lista->inicio;
         for (int j = 0; j < lista->qtd; j++) {
@@ -271,23 +264,35 @@ int algoritmoComparar (tipoLista *lista, tipoLista *complexodemais) {
                 char resultado[strlen(aux->binario)];
                 int resultadoComparador = comparador(aux->binario, aux2->binario, resultado);
                 if(resultadoComparador != -1){
-                    if (buscaLista(complexodemais, resultado) == 0) {
-                        // printf("res: %s, resU: %i\n", resultado, buscaLista(complexodemais, resultado));
-                        int somaArray[64];
-                        
-                        aux->foiUtilizado = 1;
-                        aux2->foiUtilizado = 1;
-                        mergeArray(aux->cobertura, aux2->cobertura, aux->coberturaQtd, aux2->coberturaQtd, somaArray);
-    
-                        mergeArray(globalArray, somaArray, globalArrayQtd, (aux->coberturaQtd + aux2->coberturaQtd), globalArray);
-                        globalArrayQtd = globalArrayQtd + 2;
-    
-                        adicionarLista(complexodemais, resultado, resultadoComparador, somaArray, (aux->coberturaQtd + aux2->coberturaQtd));
-                        pfvfunciona = 1;
-                        if (DEBUG_Contador == 2) {
-                            printf("%s %s %s %i [%i]\n", aux->binario, aux2->binario, resultado, resultadoComparador, DEBUG_Contador);
-                        }
-                    }
+                    int somaArray[64];
+                    
+                    mergeArray(aux->cobertura, aux2->cobertura, aux->coberturaQtd, aux2->coberturaQtd, somaArray);
+
+                    // Print the merged array
+                    // printf("Cobertura: ");
+                    // printf("{ ");
+                    // for (int i = 0; i < (aux->coberturaQtd + aux2->coberturaQtd); i++) {
+                    //     printf("%d ", somaArray[i]);
+                    // }
+                    // printf("}");
+                    // printf("\n");
+
+                    mergeArray(globalArray, somaArray, globalArrayQtd, (aux->coberturaQtd + aux2->coberturaQtd), globalArray);
+                    globalArrayQtd = globalArrayQtd + 2;
+
+                    // Print the merged array
+                    // printf("Cobertura GERAL: ");
+                    // printf("{ ");
+                    // for (int i = 0; i < globalArrayQtd; i++) {
+                    //     printf("%d ", globalArray[i]);
+                    // }
+                    // printf("}");
+                    // printf("\n");
+
+
+                    adicionarLista(complexodemais,resultado, resultadoComparador, somaArray, (aux->coberturaQtd + aux2->coberturaQtd));
+                    pfvfunciona =1;
+                    // printf("%s %s %s %i %i\n", aux->binario, aux2->binario, resultado, resultadoComparador);
                 }
             }
             aux2 = aux2->prox;
@@ -297,19 +302,11 @@ int algoritmoComparar (tipoLista *lista, tipoLista *complexodemais) {
     return pfvfunciona;
 }
 
-int buscarNaoUtilziado (tipoLista *lista) {
-    tipoNo* aux = lista->inicio;
-    for (int i = 0; i < lista->qtd; i++) {
-        if (aux->foiUtilizado != 1) {
-            printf("Elemento não utilizado: %s\n", aux->binario);
-        }
-        aux = aux->prox;
-    }
-}
+
 
 int main (){
     // "./benchmark/tests/ex00_f_da_folha.pla";
-    char nome_arquivo[] = "./benchmark/tests/ex03_funcao5var.pla"; // Tem que colocar caso ele n encontre para ler o arquivo
+    char nome_arquivo[] = "./benchmark/tests/ex04_funcao6var.pla"; // Tem que colocar caso ele n encontre para ler o arquivo
     // char nome_arquivo[256];
     // printf("Insira o nome do arquivo (com a extensao .pla no fim): \n");
     // fgets(nome_arquivo,sizeof(nome_arquivo),stdin);
@@ -323,6 +320,7 @@ int main (){
     inicializa(&listamin);
 
     int i = 0;
+    int entrada;
     while (fgets(a, sizeof(a), file) != NULL) {
         if (a[0] == '.' && a[1] == 'i') {
             entrada = a[3] - '0';
@@ -362,30 +360,23 @@ int main (){
             break;
         }
 
-        buscarNaoUtilziado(&listamin);
         liberaLista(&listamin);
         listamin = nova;
-        
+
         ordenarNum1(&listamin);
         ciclo++;
-        DEBUG_Contador++;
-        // break;
     }
-
-    tipoNo* aux = listamin.inicio;
-    for (int i=0; i < listamin.qtd; i++) {
-        printf("%s\n", aux->binario);
-        aux = aux->prox;
-    }
-
-    // printf("[");
-    // for (int i = 0; i < globalArrayQtd; i++) {
-    //     printf("%i ", globalArray[i]);
+    
+    // printf("Os mintermos %i:\n", listamin.qtd);
+    // tipoNo *aux = listamin.inicio;
+    // for (int j=0; j < listamin.qtd; j++) {
+    //     printf("%s %i\n", aux->binario, aux->numeros1);
+    //     aux = aux->prox;
     // }
-    // printf("]\n");
 
     escolhaCobertura(&listamin);
 
+    // 10-1 110- -011 1-11 -> 11-- 
 
     fclose(file);
     return 0; 
